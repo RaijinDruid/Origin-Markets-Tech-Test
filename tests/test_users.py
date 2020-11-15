73baffe2aca47aec546dee05ea0b3ca0765b7cb0
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-15 22:35:01 +0000

    Added email already exists test

--- tests/test_users.py
+++ tests/test_users.py
@@ -11,6 +11,16 @@ def test_create_user_bad_password(client):
     assert response.status_code == 400
     assert b"str type expected" in response.data and b"password" in response.data
 
+def test_email_already_exists(client):
+    user = {"email": "john123@mail.com", "password": "mysecretpassword"}
+    response = client.post('/api/v1/users', json=user)
+    assert response.status_code == 201
+    assert response.json['msg'] == "Successfully created new user"
+    response = client.post('/api/v1/users', json=user)
+    assert response.status_code == 400
+    assert b"User with email already exists" in response.data
+
+
 def test_create_user_invalid_email(client):
     user = {"email": "@mail.comSm@mail.com", "password": "pkkpokpok"}
     response = client.post('/api/v1/users', json=user)
