00d13ee6a8d1648a63fe89e0ee25a305ec8eb11d
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-15 21:25:10 +0000

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from project.app import create_app
+
+config = {
+    "SQLALCHEMY_DATABASE_URL": "sqlite:///./sqlite.db",
+    "ENV": "development",
+    "DEBUG":True,
+    "SECRET_KEY": "would-get-from-env-file"
+}
+
+app = create_app(config)
+app.run(debug=True)
