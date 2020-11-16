4ad24e7b88d24624bb7cfe832b879a6fc5d9adae
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-16 12:30:33 +0000

    Add password, decode token methods

--- project/security.py
+++ project/security.py
@@ -7,10 +7,9 @@ SECRET_KEY = current_app.config['SECRET_KEY']
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{api_version}/token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expiry = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
@@ -18,8 +17,14 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def decode_token(token):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
