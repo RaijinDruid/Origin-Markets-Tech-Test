c5e72364be34357c4bdd48b1856ee71dd0f62a9e
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-16 11:51:24 +0000

    Added Bonds, Tokens

--- project/models.py
+++ project/models.py
@@ -3,21 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 from project.database import Base
 from sqlalchemy.orm import relationship
 
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable = False, unique=True)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
 
     bonds = relationship("Bond", back_populates="owner")
+
 
 class Bond(Base):
     __tablename__ = "bonds"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable = False, unique=True)
-    password = Column(String, nullable=False)
+    isin = Column(String)
+    size = Column(Integer)
+    currency = Column(String)
+    maturity = Column(String)
+    lei = Column(String)
+    legal_name = Column(String)
+
     user_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="bonds")
--- project/schemas.py
+++ project/schemas.py
@@ -1,30 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
-class Bond(BaseModel):
-    id: int
-
-class UserBase(BaseModel):
-    email:str
-
-class UserCreate(UserBase):
-    password: str
-
-class User(UserBase):
-    id: int
-    bonds: List[Bond] = []
-    
-    class Config:
-        orm_mode = True
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
 
 class BondCreate(BaseModel):
     isin: str
@@ -33,9 +9,37 @@ class BondCreate(BaseModel):
     maturity: str
     lei: str
 
+
 class Bond(BondCreate):
     id: int
     legal_name: str
+    user_id: int
+
     class Config:
         orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    bonds: List[Bond] = []
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
