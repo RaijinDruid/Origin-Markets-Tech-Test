fc9b52f234823394e2fd126d86e71b3fa45536d0
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-16 12:31:07 +0000

    update serializer

--- project/util.py
+++ project/util.py
@@ -1,11 +1,19 @@
+from functools import wraps
+from flask import abort, request
+from collections.abc import Iterable
+from sqlalchemy.orm.collections import InstrumentedList
+
+
 def serialize_data(data, schema):
     serialized_data = []
-    if schema:
-        if type(data) != list:
-            return {k:v for (k,v) in data.__dict__.items() if k in schema.__fields__}
-        else:
-            for obj in data:
-                serialized_data.append({k:v for (k,v) in obj.__dict__.items() if k in schema.__fields__})
+    if not data:
+        return data
+    if not schema:
+        RuntimeWarning("No schema supplied to serialize data")
+        return data
+    if not isinstance(data, InstrumentedList) and not isinstance(data, list):
+        return {k: v for (k, v) in data.__dict__.items() if k in schema.__fields__}
+    else:
+        for obj in data:
+            serialized_data.append({k: v for (k, v) in obj.__dict__.items() if k in schema.__fields__})
     return serialized_data
-
-
