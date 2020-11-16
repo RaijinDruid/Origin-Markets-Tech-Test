252f25db40a5ed4cfb7631ef208aa3fd5197a632
Luke Irvine <lightningdruid@Lukes-MacBook-Pro.local>
2020-11-16 12:31:33 +0000

    remove print config

--- project/app.py
+++ project/app.py
@@ -2,20 +2,29 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 def create_app(config_obj):
-    """Create and configure an instance of the Flask application."""
+    """
+        Create and configure an instance of the Flask application.
+
+        ARGUMENTS
+        ----------------------
+        config_obj:
+            type: dict
+            description: pass in a dict with environment variables
+
+    """
     app = Flask(__name__)
     if config_obj:
         app.config.from_mapping(config_obj)
-        print(app.config)
         with app.app_context():
             from project.database import Base, engine
             from project import models
             models.Base.metadata.create_all(bind=engine)
-            from project.routes import bond_bp, user_bp
+            from project.routes import bond_bp, user_bp, token_bp
             app.register_blueprint(bond_bp)
             app.register_blueprint(user_bp)
+            app.register_blueprint(token_bp)
         return app
     else:
         raise RuntimeError("Please provide a configuration for flask application")
-
